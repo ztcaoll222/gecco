c880c570da7bc7ab3d363fbc29117547bb560093
ztcaoll222 <admin@ztcaoll222.cn>
2019-05-05 11:23:33 +0800

    更改调度接口

--- src/main/java/com/geccocrawler/gecco/scheduler/Scheduler.java
+++ src/main/java/com/geccocrawler/gecco/scheduler/Scheduler.java
@@ -4,14 +4,11 @@ import com.geccocrawler.gecco.request.HttpRequest;
 
 /**
  * 需要下载的请求都放在这里管理，可以认为这里是一个队列，保存了所有待抓取的请求。系统默认采用FIFO的方式管理请求。
- * 
- * @author huchengyi
  *
+ * @author huchengyi
  */
-public interface Scheduler {
-	
-	public HttpRequest out();
-	
-	public void into(HttpRequest request);
-	
+public interface Scheduler<T extends HttpRequest> {
+    T out();
+
+    void into(T request);
 }
