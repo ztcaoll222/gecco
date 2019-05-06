4b65ce26d397c8e3df90ccba65b0696d816ff4f3
ztcaoll222 <admin@ztcaoll222.cn>
2019-05-06 14:04:18 +0800

    格式化代码

--- src/main/java/com/geccocrawler/gecco/spider/render/RequestFieldRender.java
+++ src/main/java/com/geccocrawler/gecco/spider/render/RequestFieldRender.java
@@ -1,26 +1,22 @@
 package com.geccocrawler.gecco.spider.render;
 
-import java.lang.reflect.Field;
-import java.util.Set;
-
-import net.sf.cglib.beans.BeanMap;
-
-import org.reflections.ReflectionUtils;
-
 import com.geccocrawler.gecco.annotation.Request;
 import com.geccocrawler.gecco.request.HttpRequest;
 import com.geccocrawler.gecco.response.HttpResponse;
 import com.geccocrawler.gecco.spider.SpiderBean;
+import net.sf.cglib.beans.BeanMap;
+import org.reflections.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.util.Set;
 
 public class RequestFieldRender implements FieldRender {
 
-	@Override
-	@SuppressWarnings({"unchecked" })
-	public void render(HttpRequest request, HttpResponse response, BeanMap beanMap, SpiderBean bean) {
-		Set<Field> requestFields = ReflectionUtils.getAllFields(bean.getClass(), ReflectionUtils.withAnnotation(Request.class));
-		for(Field field : requestFields) {
-			beanMap.put(field.getName(), request);
-		}
-	}
-	
+    @Override
+    @SuppressWarnings({"unchecked"})
+    public void render(HttpRequest request, HttpResponse response, BeanMap beanMap, SpiderBean bean) {
+        Set<Field> requestFields = ReflectionUtils.getAllFields(bean.getClass(), ReflectionUtils.withAnnotation(Request.class));
+        requestFields.forEach(field -> beanMap.put(field.getName(), request));
+    }
+
 }
